286510032045b1a02f62b7045eed5f4dab792166
unknown <kulaginsergey09@gmail.com>
2021-01-23 10:57:47 +1100

    Строки в коде поехали немного в прошлом коммите, исправил

--- templates/main.php
+++ templates/main.php
@@ -31,7 +31,6 @@ function lot_price($x)
         </div>
         <ul class="lots__list">
         
-        
         <?php
         foreach ($staff as $key => $val) : ?>
         <!-- Хотел сделать всё в одном теге, но обнаруживает нежданынй endforeach, оставил так -->
@@ -47,9 +46,7 @@ function lot_price($x)
                     <div class="lot__info">
                         <span class="lot__category"><?= htmlspecialchars($val['category']); ?></span>
                         <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?= htmlspecialchars($val['name']); ?></a></h3>
-                        <div class="lot__stat
-                        
-                        e">
+                        <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
                                 <span class="lot__cost"><?= htmlspecialchars(lot_price($val['price'])); ?></span>
