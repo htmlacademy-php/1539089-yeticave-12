359e54a5cca7b2b532c4082393d4699eb6da7432
unknown <kulaginsergey09@gmail.com>
2021-03-04 21:59:15 +1100

    Создаём схему БД личного проекта

--- schema.sql
+++ schema.sql
@@ -12,84 +12,95 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
 
--- Дамп структуры базы данных yetycave2
-DROP DATABASE IF EXISTS `yetycave2`;
-CREATE DATABASE IF NOT EXISTS `yetycave2` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `yetycave2`;
+-- Дамп структуры базы данных yety
+DROP DATABASE IF EXISTS `yety`;
+CREATE DATABASE IF NOT EXISTS `yety` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `yety`;
 
--- Дамп структуры для таблица yetycave2.categories
+-- Дамп структуры для таблица yety.categories
 DROP TABLE IF EXISTS `categories`;
 CREATE TABLE IF NOT EXISTS `categories` (
-  `category_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `category_name` varchar(20) DEFAULT NULL,
   `symbol_code` varchar(20) DEFAULT NULL,
-  PRIMARY KEY (`category_id`),
+  PRIMARY KEY (`id`),
   UNIQUE KEY `unique_category` (`category_name`,`symbol_code`)
-) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8;
+
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Доски и лыжи', 'boards');
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Крепления', 'attachment');
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Ботинки', 'boots');
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Одежда', 'clothing');
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Инструменты', 'tools');
+INSERT INTO categories (category_name, symbol_code)
+VALUES ('Разное', 'other');
 
 -- Экспортируемые данные не выделены.
 
--- Дамп структуры для таблица yetycave2.lots
+-- Дамп структуры для таблица yety.users
+DROP TABLE IF EXISTS `users`;
+CREATE TABLE IF NOT EXISTS `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `registration_date` timestamp NOT NULL,
+  `email` varchar(128) DEFAULT NULL,
+  `user_name` varchar(50) DEFAULT NULL,
+  `user_password` varchar(64) DEFAULT NULL,
+  `user_contact` text,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_users` (`user_name`,`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+
+
+-- Дамп структуры для таблица yety.lots
 DROP TABLE IF EXISTS `lots`;
 CREATE TABLE IF NOT EXISTS `lots` (
-  `lot_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `date_create` timestamp NOT NULL,
   `lot_name` varchar(30) DEFAULT NULL,
   `description` varchar(128) DEFAULT NULL,
-  `image` text,
+  `image` varchar(512) DEFAULT NULL,
   `start_price` int(11) NOT NULL,
   `date_dead` timestamp NOT NULL,
   `rate_step` int(11) NOT NULL,
   `category_id` int(11) DEFAULT NULL,
   `author_id` int(11) DEFAULT NULL,
   `winner_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`lot_id`),
+  PRIMARY KEY (`id`),
   KEY `category_id` (`category_id`),
   KEY `author_id` (`author_id`),
   KEY `winner_id` (`winner_id`),
   KEY `lots_search` (`lot_name`,`description`),
-  CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`category_id`),
-  CONSTRAINT `lots_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`user_id`),
-  CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`winner_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `lots_ibfk_1` FOREIGN KEY (`category_id`) REFERENCES `categories` (`id`),
+  CONSTRAINT `lots_ibfk_2` FOREIGN KEY (`author_id`) REFERENCES `users` (`id`),
+  CONSTRAINT `lots_ibfk_3` FOREIGN KEY (`winner_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
 
--- Дамп структуры для таблица yetycave2.rates
+-- Дамп структуры для таблица yety.rates
 DROP TABLE IF EXISTS `rates`;
 CREATE TABLE IF NOT EXISTS `rates` (
-  `rate_id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `rate_date` timestamp NOT NULL,
   `rate_sum` int(11) DEFAULT NULL,
   `lot_id` int(11) DEFAULT NULL,
   `user_id` int(11) DEFAULT NULL,
-  PRIMARY KEY (`rate_id`),
+  PRIMARY KEY (`id`),
   KEY `lot_id` (`lot_id`),
   KEY `user_id` (`user_id`),
-  CONSTRAINT `rates_ibfk_1` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`lot_id`),
-  CONSTRAINT `rates_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`)
+  CONSTRAINT `rates_ibfk_1` FOREIGN KEY (`lot_id`) REFERENCES `lots` (`id`),
+  CONSTRAINT `rates_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
 
--- Дамп структуры для таблица yetycave2.users
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `user_id` int(11) NOT NULL AUTO_INCREMENT,
-  `registration_date` timestamp NOT NULL,
-  `email` varchar(128) DEFAULT NULL,
-  `user_name` varchar(50) DEFAULT NULL,
-  `user_password` varchar(64) DEFAULT NULL,
-  `user_contact` text,
-  `created_lots` int(11) DEFAULT NULL,
-  `dealed_rates` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `unique_users` (`user_name`,`user_password`),
-  KEY `created_lots` (`created_lots`),
-  KEY `dealed_rates` (`dealed_rates`),
-  CONSTRAINT `users_ibfk_1` FOREIGN KEY (`created_lots`) REFERENCES `lots` (`lot_id`),
-  CONSTRAINT `users_ibfk_2` FOREIGN KEY (`dealed_rates`) REFERENCES `rates` (`rate_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- Экспортируемые данные не выделены.
 
